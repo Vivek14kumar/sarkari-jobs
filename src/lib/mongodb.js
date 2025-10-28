bf17e5a244571bb8bae51c018e6a4676f514a464
Vivek Kumar <vivek142000kumar@gamil.com>
2025-10-28 17:54:33 +0530

    yojna

--- src/lib/mongodb.js
+++ src/lib/mongodb.js
@@ -5,24 +5,6 @@ let isConnected = false;
 export const connectToDB = async () => {
   if (isConnected) return;
 
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      dbName: "sarkari-jobs",
-    });
-    isConnected = true;
-    console.log("✅ MongoDB connected");
-  } catch (error) {
-    console.error("❌ MongoDB connection error:", error);
-  }
-};
-
-{/*import mongoose from "mongoose";
-
-let isConnected = false;
-
-export const connectToDB = async () => {
-  if (isConnected) return;
-
   const uri = process.env.MONGODB_URI; // correct env variable
   if (!uri) throw new Error("MONGODB_URI is not defined");
 
@@ -38,4 +20,4 @@ export const connectToDB = async () => {
     console.error("❌ MongoDB connection error:", error);
     throw error; // throw so API route gets the 500 error properly
   }
-};*/}
+};
