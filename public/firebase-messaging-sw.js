63057e288b27a9c63c3c8e3b0e0a1defdb306a7a
Vivek Kumar <vivek142000kumar@gamil.com>
2025-11-16 09:17:28 +0530

    notifi sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,11 +1,11 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
+importScripts("https://www.gstatic.com/firebasejs/12.6.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.6.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyB59UYg3ERnPAfhZXM0NR6_2boQzp_amh4",
   authDomain: "results-hub-job.firebaseapp.com",
   projectId: "results-hub-job",
-  storageBucket: "results-hub-job.appspot.com",
+  storageBucket: "results-hub-job.firebasestorage.app",
   messagingSenderId: "949728586046",
   appId: "1:949728586046:web:bafd0e6a281c9a4f2b246f"
 });
@@ -13,12 +13,15 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
+  console.log("[SW] Background message:", payload);
+
   const title = payload.notification?.title || "Resultshub";
   const options = {
     body: payload.notification?.body || "",
     icon: "/icons/icon-192.png",
     data: payload.data || {},
   };
+
   self.registration.showNotification(title, options);
 });
 
